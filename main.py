07b6035a02f8b130f9d7172745eed817ec9fd093
Nicholas Schreiner <nicokz>
2024-10-23 22:40:01 +0300

    comments added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+"""
+===
+= RP2040 LCD 1.28 screen rotation
+===
+
+Type:      code example
+Author:    Nicholas Schreiner a.k.a. LighthunterWS
+Revision:  0.1.0 PA
+"""
 import watch.RP2040watch as Watch
 import time
 
-LCD = Watch.GC9A01()
+LCD = Watch.GC9A01() # init GC9A01
 LCD.set_bl_pwm(65535) # to maximum! (TODO: play to change brightness)
 LCD.fill(LCD.black)
 LCD.fill_rect(0,0,240,40,LCD.red)
@@ -12,28 +21,28 @@ LCD.line(120, 45, 123, 55, 0xffff)
 LCD.hline(115, 120, 10, 0xffff)
 LCD.show()
 time.sleep(2)
-LCD.orient(Watch.NORTH)
+LCD.orient(Watch.NORTH) # rotate NORTH (virtually normal handy watch position)
 LCD.fill_rect(0,0,240,40,LCD.red)
 LCD.text("NORTH",60,25,LCD.green)
 LCD.show()
 time.sleep(0.8)
 LCD.orient(Watch.WEST)
 LCD.fill_rect(0,0,240,40,LCD.red)
-LCD.text("WEST",60,25,LCD.green)
+LCD.text("WEST",60,25,LCD.green) # rotate 90 degree to left of NORTH
 LCD.show()
 time.sleep(0.8)
 LCD.orient(Watch.SOUTH)
 LCD.fill_rect(0,0,240,40,LCD.red)
-LCD.text("SOUTH",60,25,LCD.green)
+LCD.text("SOUTH",60,25,LCD.green) # rotate 180 degree out of NORTH (opposit position)
 LCD.show()
 time.sleep(0.8)
 LCD.orient(Watch.EAST)
-LCD.fill_rect(0,0,240,40,LCD.red)
+LCD.fill_rect(0,0,240,40,LCD.red) # rotate 90 degree to right of NORTH
 LCD.text("EAST",60,25,LCD.green)
 LCD.show()
 time.sleep(0.8)
 LCD.orient(Watch.NORTH)
-LCD.fill_rect(0,0,240,40,0x00FF)
+LCD.fill_rect(0,0,240,40,0x00FF) # rotate back to NORTH
 LCD.text("NORTH",60,25,0xFF00)
-                      #0xffff
+                      #0xffff ?
 LCD.show()
